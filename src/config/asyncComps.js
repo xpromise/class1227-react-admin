43fe01a39b353e5fd468f4b33b6d45d5730f3557
xiongjian <webjsforyou@163.com>
2020-06-08 11:35:03 +0800

    branch: 添加day01开发分支

--- src/config/asyncComps.js
+++ src/config/asyncComps.js
@@ -18,7 +18,6 @@ const AddOrUpdateRole = () =>
 const Chapter = () => lazy(() => import("@pages/Edu/Chapter"));
 const Comment = () => lazy(() => import("@pages/Edu/Comment"));
 const Course = () => lazy(() => import("@pages/Edu/Course"));
-const Subject = () => lazy(() => import("@pages/Edu/Subject"));
 const Teacher = () => lazy(() => import("@pages/Edu/Teacher"));
 const Settings = () => lazy(() => import("@pages/User/Settings"));
 const Center = () => lazy(() => import("@pages/User/Center"));
@@ -35,7 +34,6 @@ export default {
   Chapter,
   Comment,
   Course,
-  Subject,
   Teacher,
   Settings,
   Center,
